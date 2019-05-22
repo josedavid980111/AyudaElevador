849a420441ecbadfee6a41e602ecd648a04e4092
josedavid980111 <35538108+josedavid980111@users.noreply.github.com>
2019-05-22 01:35:33 -0500

    Funcionando

---
+++ Ayuda/Ayuda/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Ayuda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.Timer3 = new System.Windows.Forms.Timer(this.components);
+            this.Timer4 = new System.Windows.Forms.Timer(this.components);
+            this.Timer5 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Timer1
+            // 
+            this.Timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(284, 347);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "button1";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(63, 347);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(35, 13);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "label1";
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(284, 282);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(75, 23);
+            this.Button2.TabIndex = 2;
+            this.Button2.Text = "button2";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(284, 217);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(75, 23);
+            this.Button3.TabIndex = 3;
+            this.Button3.Text = "button3";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button4
+            // 
+            this.Button4.Location = new System.Drawing.Point(284, 152);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(75, 23);
+            this.Button4.TabIndex = 4;
+            this.Button4.Text = "button4";
+            this.Button4.UseVisualStyleBackColor = true;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Button5
+            // 
+            this.Button5.Location = new System.Drawing.Point(284, 87);
+            this.Button5.Name = "Button5";
+            this.Button5.Size = new System.Drawing.Size(75, 23);
+            this.Button5.TabIndex = 5;
+            this.Button5.Text = "button5";
+            this.Button5.UseVisualStyleBackColor = true;
+            this.Button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Button5);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Timer Timer2;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Button Button5;
+        private System.Windows.Forms.Timer Timer3;
+        private System.Windows.Forms.Timer Timer4;
+        private System.Windows.Forms.Timer Timer5;
+    }
+}
+
---
+++ Ayuda/Ayuda/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ayuda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vieja = 1;
+            nueva = 1;
+            x = 63;
+            y = 347;
+            contador = 0;
+            Button1.BackColor = Color.Red;
+            Button2.BackColor = Color.CadetBlue;
+            Button3.BackColor = Color.CadetBlue;
+            Button4.BackColor = Color.CadetBlue;
+            Button5.BackColor = Color.CadetBlue;
+        }
+
+        int a, vieja, nueva, contador, x, y;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nueva = 1;
+            Timer1.Interval = 5;
+            Timer1.Start();
+            Button1.BackColor = Color.CadetBlue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            nueva = 2;
+            Timer1.Start();
+            Timer1.Interval = 5;
+            Button2.BackColor = Color.CadetBlue;
+        }
+
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            nueva = 3;
+            Timer1.Start();
+            Timer1.Interval = 5;
+            Button3.BackColor = Color.CadetBlue;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            nueva = 4;
+            Timer1.Start();
+            Timer1.Interval = 5;
+            Button3.BackColor = Color.CadetBlue;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            nueva = 5;
+            Timer1.Start();
+            Timer1.Interval = 5;
+            Button5.BackColor = Color.CadetBlue;
+        }
+        
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nueva > vieja)
+            {
+                contador = (nueva - vieja) * 65;
+                a = a + 1;
+                Label1.Location = new Point(x, y - a);
+                if (a == contador)
+                {
+                    Timer1.Stop();
+                    y = y - a;
+                    a = 0;
+                    if (nueva == 1)
+                    {
+                        Button1.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 2)
+                    {
+                        Button2.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 3)
+                    {
+                        Button3.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 4)
+                    {
+                        Button4.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 5)
+                    {
+                        Button5.BackColor = Color.Red;
+                    }
+
+
+
+
+
+                    if (vieja == 1)
+                    {
+                        Button1.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 2)
+                    {
+                        Button2.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 3)
+                    {
+                        Button3.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 4)
+                    {
+                        Button4.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 5)
+                    {
+                        Button5.BackColor = Color.CadetBlue;
+                    }
+
+
+
+
+                    vieja = nueva;
+                }
+            }
+
+            if (nueva < vieja)
+            {
+                contador = (vieja - nueva) * 65;
+                a = a + 1;
+                Label1.Location = new Point(x, y + a);
+                if (a == contador)
+                {
+                    Timer1.Stop();
+                    y = y + a;
+                    a = 0;
+                    if (nueva == 1)
+                    {
+                        Button1.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 2)
+                    {
+                        Button2.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 3)
+                    {
+                        Button3.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 4)
+                    {
+                        Button4.BackColor = Color.Red;
+                    }
+
+                    else if (nueva == 5)
+                    {
+                        Button5.BackColor = Color.Red;
+                    }
+
+
+
+
+
+                    if (vieja == 1)
+                    {
+                        Button1.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 2)
+                    {
+                        Button2.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 3)
+                    {
+                        Button3.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 4)
+                    {
+                        Button4.BackColor = Color.CadetBlue;
+                    }
+
+                    else if (vieja == 5)
+                    {
+                        Button5.BackColor = Color.CadetBlue;
+                    }
+
+
+
+                    vieja = nueva;
+                }
+            }
+
+
+        }
+
+
+    }
+
+
+}
